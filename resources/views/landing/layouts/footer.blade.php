4bdf300c42e1ae7b03dc99c5602a539390bdb81c
achmadtaufikhidayatullah <achmadtaufikhidayatullah6@gmail.com>
2023-04-05 16:43:45 +0800

    floating whatsapp, button login, content footer

--- resources/views/landing/layouts/footer.blade.php
+++ resources/views/landing/layouts/footer.blade.php
@@ -1,40 +1,44 @@
 <footer id="footer">
     <div class="footer-top">
-      <div class="container">
-        <div class="row">
+        <div class="container">
+            <div class="row">
 
-          <div class="col-lg-4 col-md-6">
-            <div class="footer-info">
-              <h3><span>Metapropolis</span></h3>
-              <p>
-                Lt. 3A, Grand ITC Permata Hijau Emerald 25, <br>
-                 Jl. Arteri Permata Hijau No.11, RT.11/RW.10, Grogol Utara, <br>
-                 Kec. Kby. Lama, jakarta, Daerah Khusus Ibukota Jakarta 12210 <br><br>
-                <strong>Phone:</strong> +62 812-1239-2752<br>
-                <strong>Email:</strong> info@example.com<br>
-              </p>
-              <div class="social-links mt-3">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-info">
+                        <h3><span>Metapropolis</span></h3>
+                        <p>
+                            Grand ITC Permata Hijau <br>
+                            Jl.Arterti Permata Hijau <br>
+                            Jakarta Selatan 12210 <br><br>
+                            <strong>Phone:</strong> +62 812-1239-2752<br>
+                            <strong>Email:</strong> info@metapropolis.id<br>
+                        </p>
+                        {{-- <div class="social-links mt-3">
                 <a href="https://www.facebook.com/profile.php?id=100091457464973" class="facebook"><i class="bx bxl-facebook"></i></a>
                 <a href="https://www.youtube.com/@metapropolis3536" class="youtube"><i class="bx bxl-youtube"></i></a>
                 <a href="https://www.instagram.com/metapropolis/" class="instagram"><i class="bx bxl-instagram"></i></a>
                 <a href="https://www.tiktok.com/@metabiolifeindonesia" class="tiktok"><i class="bx bxl-tiktok"></i></a>
-              </div>
-            </div>
-          </div>
+              </div> --}}
+                    </div>
+                </div>
 
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Link</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{url('/')}}">Beranda</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('tentang_kami')}}">Tentang Kami</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('jurnal')}}">Jurnal</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://metapropolis.resellr.id/register-reseller">Join Resellr</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://metapropolis.resellr.id/register-dropshipper?id=26480">Join Dropshipper</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="{{route('faq')}}">Faq</a></li>
-            </ul>
-          </div>
+                <div class="col-lg-4 col-md-6 footer-links">
+                    <h4>Link</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{url('/')}}">Beranda</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{route('tentang_kami')}}">Tentang Kami</a>
+                        </li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{route('jurnal')}}">Jurnal</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a
+                                href="https://metapropolis.resellr.id/register-reseller">Join Resellr</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a
+                                href="https://metapropolis.resellr.id/register-dropshipper?id=26480">Join
+                                Dropshipper</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{route('faq')}}">Faq</a></li>
+                    </ul>
+                </div>
 
-          {{-- <div class="col-lg-3 col-md-6 footer-links">
+                {{-- <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
@@ -45,57 +49,80 @@
             </ul>
           </div> --}}
 
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Follow Us</h4>
-            <h5>
-              
-             {{-- <a href="#"><span class="fa fa-facebook"> Facebook meta</span></a> <br><br> --}}
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Follow Us</h4>
+                    {{-- <h5>
              <a href="https://www.instagram.com/metapropolis/"><span class="fa fa-instagram"> Instagram meta</span></a> <br><br>
              <a href="https://www.youtube.com/@metapropolis3536"><span class="fa fa-youtube"> Youtube meta</span></a> <br><br>
              <a href="https://www.facebook.com/profile.php?id=100091457464973"><span class="fa-brands fa-facebook"> Facebook meta</span></a> <br><br>
              <a href="https://www.tiktok.com/@metabiolifeindonesia"><span class="fa-brands fa-tiktok"> Tiktok meta</span></a> <br>
-            </h5>
-            {{-- <form action="" method="post">
+            </h5> --}}
+                    <div class="social-links mt-3">
+                        <a href="https://www.facebook.com/profile.php?id=100091457464973" class="facebook"><i
+                                class="bx bxl-facebook"></i></a>
+                        <a href="https://www.youtube.com/@metapropolis3536" class="youtube"><i
+                                class="bx bxl-youtube"></i></a>
+                        <a href="https://www.instagram.com/metapropolis/" class="instagram"><i
+                                class="bx bxl-instagram"></i></a>
+                        <a href="https://www.tiktok.com/@metabiolifeindonesia" class="tiktok"><i
+                                class="bx bxl-tiktok"></i></a>
+                    </div>
+                    {{-- <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form> --}}
 
-          </div>
+                </div>
 
+            </div>
         </div>
-      </div>
     </div>
 
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Metapropolis</span></strong> All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-        Designed by <a href="#">Metapropolis</a>
-      </div>
+        <div class="copyright">
+            &copy; Copyright <strong><span>Metapropolis</span></strong> All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
+            Designed by <a href="#">Metapropolis</a>
+        </div>
     </div>
-  </footer><!-- End Footer -->
+</footer><!-- End Footer -->
 
-  {{-- <div id="preloader"></div> --}}
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+{{-- <div id="preloader"></div> --}}
+{{-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a> --}}
 
-  <!-- Vendor JS Files -->
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-  <script src="{{asset('landing/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
-  <script src="{{asset('landing/assets/vendor/php-email-form/validate.js')}}"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-  {{-- FONT AWESOME --}}
-  <script src="https://kit.fontawesome.com/c1ac2a479a.js" crossorigin="anonymous"></script>
+{{-- FLOATING WHATSAPP --}}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=6281212392752&text=Hallo%20Metapropolis"
+    class="float" target="_blank">
+    <i class="fa fa-whatsapp my-float"></i>
+</a>
 
-  <!-- Template Main JS File -->
-  <script src="{{asset('landing/assets/js/main.js')}}"></script>
+
+
+<!-- Vendor JS Files -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"
+    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script src="{{asset('landing/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/aos/aos.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/php-email-form/validate.js')}}"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+{{-- FONT AWESOME --}}
+<script src="https://kit.fontawesome.com/c1ac2a479a.js" crossorigin="anonymous"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('landing/assets/js/main.js')}}"></script>
--- resources/views/landing/layouts/navbar.blade.php
+++ resources/views/landing/layouts/navbar.blade.php
@@ -1,14 +1,23 @@
 <nav id="navbar" class="navbar order-last order-lg-0">
     <ul>
-      <li><a class="nav-link scrollto {{ request()->is('/') ? 'active' : ''}}" href="{{url('/')}}">Beranda</a></li>
-      <li><a class="nav-link scrollto {{ request()->is('tentang_kami') ? 'active' : ''}}" href="{{route('tentang_kami')}}">Tentang Kami</a></li>
-      <li><a class="nav-link scrollto {{ request()->is('jurnal') ? 'active' : ''}}" href="{{route('jurnal')}}">Jurnal</a></li>
-      {{-- <li><a class="nav-link scrollto" href="{{route('register')}}">Join Resellr</a></li> --}}
-      <li><a class="nav-link scrollto" href="https://metapropolis.resellr.id/register-reseller">Join Resellr</a></li>
-      {{-- <li><a class="nav-link scrollto" href="{{route('register_dropshipper')}}">Join Dropshipper</a></li> --}}
-      <li><a class="nav-link scrollto" href="https://metapropolis.resellr.id/register-dropshipper?id=26480">Join Dropshipper</a></li>
-      <li><a class="nav-link scrollto {{ request()->is('faq') ? 'active' : ''}}" href="{{route('faq')}}">Faq</a></li>
-      {{-- <li><a class="nav-link scrollto" href="#">Kontak</a></li> --}}
+        <li><a class="nav-link scrollto {{ request()->is('/') ? 'active' : ''}}" href="{{url('/')}}">Beranda</a></li>
+        <li><a class="nav-link scrollto {{ request()->is('tentang_kami') ? 'active' : ''}}"
+                href="{{route('tentang_kami')}}">Tentang Kami</a></li>
+        <li><a class="nav-link scrollto {{ request()->is('jurnal') ? 'active' : ''}}"
+                href="{{route('jurnal')}}">Jurnal</a></li>
+        {{-- <li><a class="nav-link scrollto" href="{{route('register')}}">Join Resellr</a></li> --}}
+        <li><a class="nav-link scrollto" href="https://metapropolis.resellr.id/register-reseller">Join Resellr</a></li>
+        {{-- <li><a class="nav-link scrollto" href="{{route('register_dropshipper')}}">Join Dropshipper</a></li> --}}
+        <li><a class="nav-link scrollto" href="https://metapropolis.resellr.id/register-dropshipper?id=26480">Join
+                Dropshipper</a></li>
+        <li><a class="nav-link scrollto {{ request()->is('faq') ? 'active' : ''}}" href="{{route('faq')}}">Faq</a></li>
+        {{-- <li><a class="nav-link scrollto" href="#">Kontak</a></li> --}}
+        <li class="dropdown"><a href="#"><span>Login</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+                <li><a href="https://metapropolis.resellr.id/login-reseller">Login Reseller</a></li>
+                <li><a href="https://metapropolis.resellr.id/login-dropshipper">Login Dropshipper</a></li>
+            </ul>
+        </li>
     </ul>
     <i class="bi bi-list mobile-nav-toggle"></i>
-  </nav><!-- .navbar -->
+</nav><!-- .navbar -->
