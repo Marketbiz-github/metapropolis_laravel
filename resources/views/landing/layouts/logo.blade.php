dbf32d487d83ef7cb1811cd9c8f0e3e5aff6b687
dwgdbima <dewagedebima@gmail.com>
2023-06-22 17:06:26 +0800

    logo

--- resources/views/landing/layouts/logo.blade.php
+++ resources/views/landing/layouts/logo.blade.php
@@ -1,1 +1,1 @@
-    <img class="logo me-auto me-lg-0" src="{{asset('image_propolis/logo-meta.png')}}" width="95" height="auto" alt="logo" srcset="">
+    <img class="logo me-auto me-lg-0" src="{{asset('image_propolis/Logo-meta.png')}}" width="95" height="auto" alt="logo" srcset="">
